7bbae847aee38bd3209f727ba4bc5a56ef2b25a5
Kirper <89360103+Kirper@users.noreply.github.com>
2022-11-19 23:26:12 +0300

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ document.querySelector("#button").onclick = function(){
   if (clicks > 50 && clicks < 69) {
     document.getElementById("najat").innerHTML = " хватит!!! уже " + clicks + " раз";
   } else if (clicks > 69 && clicks < 100) {
-    document.getElementById("najat").innerHTML = " ты не заебался??? уже" + clicks;
+    document.getElementById("najat").innerHTML = " ты не устал??? уже" + clicks;
   } else if (clicks == 100) {
     document.getElementById("najat").innerHTML = " не зли меня, я все обнуляю)";
     clicks = 0
